55c7cd231102254beed27b7cf1dbb153f3b1304d
simgia <josego@simgia.com>
2017-04-05 15:47:56 -0400

    Se mejoro la parte visual de special reports

--- single-special-reports.php
+++ single-special-reports.php
@@ -9,8 +9,9 @@
 		</div>
 	</div>
 	<article id="single-post">
-		<section id="content">
-			<div class="container">
+		<section>
+			<div class="container-fluid special-report-style">
+
 				<div class="row post-header">
 					<div class="twelve columns">
 						<h1><?php the_title(); ?></h1>
@@ -24,19 +25,8 @@
 						<p class="post-excerpt"><?php the_excerpt(); ?></p>
 					</div>
 				</div>
-				<div class="row post-meta">
-					<div class="ten offset-by-one columns ">
-						<div class="one-third column"><span class="icon_pencil"></span>
-						<span class="info"><b>
-							<?php echo get_the_date(); ?>
-							<?php echo get_the_term_list( $post->ID, 'publisher', ' | ', ', ' ); ?>
-							<?php echo ' <a> | </a>'; ?>
-							<?php $author_name =  the_author_posts_link( $user_id ); echo $author_name; ?>
-						</b></span></div>
-						<div class="one-third column"><span class="icon_pin_alt"></span><span class="info"><b><?php echo get_the_term_list( $post->ID, 'country', ' ', ', ' ); ?></b></span></div>
-						<div class="one-third column"><span class="icon_tag_alt"></span><span class="info"><b><?php echo get_the_term_list( $post->ID, 'topic', ' ', ', ' ); ?></b></span></div>
-					</div>
-				</div>
+
+				
 				<div class="row ">
 					<!-- <div class="post-content ten offset-by-one columns"> -->
                         <!-- Show map depending on a variable -->
@@ -90,6 +80,24 @@
 							});
 						});
 					</script>
+				</div>
+
+				<div class="container">
+					<div class="row post-meta">
+					<div class="ten offset-by-one columns ">
+						<div class="one-third column">
+						<span class="icon_pencil"></span>
+						<span class="info"><b>
+							<?php echo get_the_date(); ?>
+							<?php echo get_the_term_list( $post->ID, 'publisher', ' | ', ', ' ); ?>
+							<?php echo ' <a> | </a>'; ?>
+							<?php $author_name =  the_author_posts_link( $user_id ); echo $author_name; ?>
+						</b></span>
+						</div>
+						<div class="one-third column"><span class="icon_pin_alt"></span><span class="info"><b><?php echo get_the_term_list( $post->ID, 'country', ' ', ', ' ); ?></b></span></div>
+						<div class="one-third column"><span class="icon_tag_alt"></span><span class="info"><b><?php echo get_the_term_list( $post->ID, 'topic', ' ', ', ' ); ?></b></span></div>
+					</div>
+				</div>
 				</div>
 			</div>
 		</section>
